f3527290472dddeafc53021631121e8f8e7032b7
madalinefitzpatrick <madaline.fitzpatrick@gmail.com>
2023-03-13 11:41:42 -0400

    lab completed with help

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,11 @@
-#!/usr/bin/env python3
+
 
 def print_fibonacci(length):
-    pass
+    fibonacci_list = []
+    for n in range(length):
+        fibonacci_list.append(n)
+        if n >= 2:
+            fibonacci_list[n] = fibonacci_list[n-2] + fibonacci_list[n-1]
+        
+    print(fibonacci_list)
+        
